add58d8df83555dddea4c85d0e329e556209df3a
Sirazad <lajosimola2@gmail.com>
2021-04-07 11:17:41 +0200

    do small refactoring

--- src/main/java/com/sirazad/robots/Main.java
+++ src/main/java/com/sirazad/robots/Main.java
@@ -12,16 +12,9 @@ public class Main {
     public static void main(String[] args) {
         Storage depo = new Storage();
         DaoManager manager = new DaoManager();
-        List<Robots> workers = new ArrayList();
-        workers = manager.getData();
+        List<Robots> workers = manager.getData();
 
-        /*        workers.add(new Mac("R2", 4));
-        workers.add(new Eco("3CR", 3));
-        workers.add(new Pro("BB8", 10));
-        workers.add(new Eco("E12", 4));
-*/
         int[][] logisticPlan = depo.createLogisticPlan();
-
         depo.simulateOneDay(workers);
         depo.simulateOneDay(workers);
         depo.simulateOneDay(workers);
